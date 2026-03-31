793d7e6b878c5ec2488e078c6a671d09a3fbe7f1
shkarman12 <shaikharman79000@gmail.com>
2026-03-31 22:05:24 +0530

    JAAVA CHANGED

--- backend.groovy
+++ backend.groovy
@@ -15,13 +15,13 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/armanshaikh98713-create/Java-backend.git'
             }
         }
-        stage('Debug Java') {
-            steps {
-                sh 'echo $JAVA_HOME'
-                sh 'java -version'
-                sh 'mvn -version'
-            }
-        }
+        // stage('Debug Java') {
+        //     steps {
+        //         sh 'echo $JAVA_HOME'
+        //         sh 'java -version'
+        //         sh 'mvn -version'
+        //     }
+        // }
         stage('Building Code') {
             steps {
                 sh 'mvn clean package'
