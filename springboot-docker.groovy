abd61f414746cbc319e15820919f5f0c02bb88f4
shkarman12 <shaikharman79000@gmail.com>
2026-04-09 22:26:40 +0530

    changes

--- springboot-docker.groovy
+++ springboot-docker.groovy
@@ -21,6 +21,7 @@ pipeline {
                 sshagent(['Docker']){
                     sh "scp -o  StrictHostKeyChecking=no -r * ubuntu@3.108.185.143:/home/ubuntu"
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker build -t java-docker /home/ubuntu/.' " 
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker stop docker-java' " 
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker rm docker-java' " 
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker run -d -p 7861:8080 --name docker-java java-docker' " 
                 }
