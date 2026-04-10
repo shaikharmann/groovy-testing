a4bad4d11fec25a39ca7f25bcf85084e16739866
shkarman12 <shaikharman79000@gmail.com>
2026-04-10 22:04:28 +0530

    NEW CHANGES IN PUSH

--- springboot-docker.groovy
+++ springboot-docker.groovy
@@ -16,14 +16,21 @@ pipeline {
             }
         }
 
-        stage('sending code'){
+        stage('Build N Deploy'){
             steps{
                 sshagent(['Docker']){
                     sh "scp -o  StrictHostKeyChecking=no -r * ubuntu@3.108.185.143:/home/ubuntu"
-                    sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker build -t java-docker /home/ubuntu/.' " 
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker build -t shkarman12/java-docker /home/ubuntu/.' " 
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker stop docker-java' " 
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker rm docker-java' " 
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker run -d -p 7861:8080 --name docker-java java-docker' " 
+                }
+        } }
+
+        stage('Pushing to DockerHub'){
+            steps{
+                sshagent(['Docker']){
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@3.108.185.143 'docker push shkarman12/java-docker:v.${BUILD_ID}' " 
                 }
         } }
 
