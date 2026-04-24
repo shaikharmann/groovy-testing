b7b7de989bd65753f1ae3a0f39f0073953258545
shkarman12 <shaikharman79000@gmail.com>
2026-04-24 20:34:52 +0530

    changes

--- springboot-docker.groovy
+++ springboot-docker.groovy
@@ -25,7 +25,7 @@ pipeline {
                     sh "scp -o  StrictHostKeyChecking=no -r * ubuntu@${IP}:/home/ubuntu"
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@${IP} 'docker build -t shkarman12/java-docker:v.${BUILD_ID} /home/ubuntu/.' " 
                     // sh "ssh -o StrictHostKeyChecking=no ubuntu@${IP} 'docker stop docker-java' " 
-                    sh "ssh -o StrictHostKeyChecking=no ubuntu@${IP} 'docker rm docker-java' "
+                    // sh "ssh -o StrictHostKeyChecking=no ubuntu@${IP} 'docker rm docker-java' "
                     sh "ssh -o StrictHostKeyChecking=no ubuntu@${IP} 'docker run -d -p 7861:8080 --name docker-java java-docker' " 
                 }
         } }
